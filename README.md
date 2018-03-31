bb8a274a6842193043d92ff6bba9c47d4521f1f2
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-03-31 22:42:31 +0200

    sycn + wait

--- README.md
+++ README.md
@@ -23,7 +23,6 @@ Setting up mysql instance:
         -p 3306 \
         mysql/mysql-server:5.7
 
-
     MYSQL_HOST=`docker inspect ${MYSQL_CONTAINER_NAME} \
        --format='{{.NetworkSettings.IPAddress}}'`
 
@@ -33,17 +32,58 @@ Setting up mysql instance:
         $MYSQL_DATABASE \
         < db/schema.sql
 
+If you don't have a mysql client, you can do it from inside the container:
 
-TODO: add parameters for populating the task table
+    docker exec -i mysql mysql \
+        -u"$MYSQL_USER" \
+        -p"$MYSQL_PASSWORD" \
+        $MYSQL_DATABASE \
+        < db/schema.sql
+
+Now run the app.
+
+First add the tasks:
+
+    docker run -it --rm \
+        -e MYSQL_HOST="$MYSQL_HOST" \
+        -e MYSQL_DATABASE="$MYSQL_DATABASE" \
+        -e MYSQL_USER="$MYSQL_USER" \
+        -e MYSQL_PASSWORD="$MYSQL_PASSWORD" \
+        codeforces-scraper get-tasks
+
+Next, let it scrape:
+
+    docker run --rm -d \
+        --name codeforces-scraper-0 \
+        -e MYSQL_HOST="$MYSQL_HOST" \
+        -e MYSQL_DATABASE="$MYSQL_DATABASE" \
+        -e MYSQL_USER="$MYSQL_USER" \
+        -e MYSQL_PASSWORD="$MYSQL_PASSWORD" \
+        -e NUM_EXECUTORS="6" \
+        codeforces-scraper scrape
+
+    docker run --rm  \
+        --name codeforces-scraper-1 \
+        -e MYSQL_HOST="$MYSQL_HOST" \
+        -e MYSQL_DATABASE="$MYSQL_DATABASE" \
+        -e MYSQL_USER="$MYSQL_USER" \
+        -e MYSQL_PASSWORD="$MYSQL_PASSWORD" \
+        -e NUM_EXECUTORS="6" \
+        codeforces-scraper scrape
+
+You can see the logs:
+
+
+
+
 
 Running
 
 
 
-
 ## Dependencies
 
-It already has geckodriver in bin. It was downloaded this way:
+It already has `geckodriver` in `bin`. It was downloaded this way:
 
     # from https://github.com/mozilla/geckodriver/releases
     wget https://github.com/mozilla/geckodriver/releases/download/v0.20.0/geckodriver-v0.20.0-linux64.tar.gz
--- src/main/java/codeforcescrawl/CodeExtractor.java
+++ src/main/java/codeforcescrawl/CodeExtractor.java
@@ -4,8 +4,10 @@ import com.google.common.collect.Queues;
 import com.google.common.io.Closer;
 import org.openqa.selenium.WebDriver;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -26,28 +28,43 @@ public class CodeExtractor {
         ExecutorService executor = Executors.newFixedThreadPool(numExecutors);
         Closer closer = Closer.create();
 
+        CountDownLatch latch = new CountDownLatch(numExecutors);
+
+        List<Runnable> tasks = new ArrayList<>(numExecutors);
         for (int i = 0; i < numExecutors; i++) {
             WebDriver driver = Factory.createFirefoxDriver();
             CodeforcesScraper scraper = new CodeforcesScraper(driver, db);
-
-            executor.submit(new CodeExtractorRunnable(queue, scraper));
-
             closer.register(scraper);
+
+            tasks.add(new CodeExtractorRunnable(queue, latch, scraper));
+        }
+
+        System.out.println("waiting 15 seconds for the drivers to start...");
+        Thread.sleep(15000);
+
+        for (Runnable task : tasks) {
+            executor.submit(task);
         }
 
         executor.shutdown();
         executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
 
+        System.out.println("waiting for tasks to finish...");
+        latch.await();
+
+        System.out.println("done, can shut down safely");
         closer.close();
     }
 
     private static class CodeExtractorRunnable implements Runnable {
 
         private final Queue<String> queue;
+        private final CountDownLatch latch;
         private final CodeforcesScraper scraper;
 
-        public CodeExtractorRunnable(Queue<String> queue, CodeforcesScraper scraper) {
+        public CodeExtractorRunnable(Queue<String> queue, CountDownLatch latch, CodeforcesScraper scraper) {
             this.queue = queue;
+            this.latch = latch;
             this.scraper = scraper;
         }
 
@@ -62,6 +79,8 @@ public class CodeExtractor {
                     continue;
                 }
             }
+
+            latch.countDown();
         }
     }
 
